e6dc2330e0bfa8064b74e504462b4115ee3c43e4
aleckshen <aleckshn@gmail.com>
2026-01-03 18:52:05 +1300

    uibuild

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => {
   if (req.method === 'POST') {
@@ -98,7 +101,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
