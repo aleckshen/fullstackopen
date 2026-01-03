c7e862beeb42939128e1b3b1e30daf4d75027bfa
aleckshen <aleckshn@gmail.com>
2026-01-03 17:32:14 +1300

    uibuild

--- part3/notes/frontend/vite.config.js
+++ part3/notes/frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy:  {
+      '/api': {
+        target: 'https://fullstackopen-2urr.onrender.com/',
+        changeOrigin: true,
+      }
+    }
+  }
 })
